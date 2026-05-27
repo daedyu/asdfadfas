f0fef63a7a345b9b712a8d2a40a3595a5bc1f58e
kmg <legolove08@naver.com>
2026-05-27 09:55:55 +0900

    init: 커밋

---
+++ src/main/java/com/dgsw/fshiuhiwfeihu/FshiuhiwfeihuApplication.java
@@ -0,0 +1,13 @@
+package com.dgsw.fshiuhiwfeihu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FshiuhiwfeihuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FshiuhiwfeihuApplication.class, args);
+    }
+
+}
